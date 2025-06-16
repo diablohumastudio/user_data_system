ac470bbc800f3a0a2361b06c1a3f743eb14caac7
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-16 16:24:06 -0500

    inventory name changed to allies_inventory

--- global_systems/user_data_system/user_data.gd
+++ global_systems/user_data_system/user_data.gd
@@ -1,7 +1,7 @@
 class_name UserData extends Resource
 
 @export_storage var user_name: String
-@export_storage var inventory: UserAllyInventory = UserAllyInventory.new()
+@export_storage var allies_inventory: UserAllyInventory = UserAllyInventory.new()
 @export_storage var progress: UserProgress = UserProgress.new()
 @export_storage var achievements: UserAchievements = UserAchievements.new()
 @export_storage var settings: UserSettings = UserSettings.new()

c90067fd4588fc7d9ee782fe3e61dadb4bd2e3c7
diablohumastudioadmin <admin@diablohumastudio.com>
2025-06-22 16:51:05 -0500

    fix for error when setted before UDS ready

--- data/levels/models/level.gd
+++ data/levels/models/level.gd
@@ -5,6 +5,7 @@ class_name Level extends Resource
 
 func _set_lv_pr(new_value: LevelProgress):
 	progress = new_value
+	if !UDS.user_data: return
 	for saved_lv_pr in UDS.user_data.progress.level_progresses:
 		if saved_lv_pr.id == progress.id:
 			progress = saved_lv_pr
